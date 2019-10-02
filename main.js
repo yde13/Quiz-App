3ee410d8a374177454ddadbcd3e6e750acd06d88
yde <philip.ydehed@gmail.com>
2019-10-02 16:32:07 +0200

    hide display

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 //spara värdet noofquestion och skriva ut så många frågor som spelare vill ha
-//spara värde så man ser score i slutet och hur många frågor man svarat på
 //kunna trycka på checkboxes som sparas nånstans(checked)
+//kolla ifall checkboxes är true eller false
+//spara värde så man ser score i slutet 
+
 
 class Quiz {
     constructor(name) {
@@ -9,7 +11,7 @@ class Quiz {
         this.noOfQuestions = noOfQuestions;
         this.quests = [];
         this.currentQuestion = 0;
-
+        this.score = 0;
     }
     setName(){
         this.name = document.getElementById("name").value;
@@ -17,9 +19,10 @@ class Quiz {
     setnoOfQuestions(){
         this.noOfQuestions = document.getElementById("noOfQuestions").value;
     }
+    nextQuestion(){
+        this.currentQuestion++
+    }
 }
-
-
 
 class Question {
     constructor(category, question, answer) {
@@ -38,7 +41,8 @@ class Question {
 
 function display() {
     document.getElementById("display").style.display = "none";
-  }
+    document.getElementById('choices').classList.remove('hidden');
+}
 
 let json = getJSON("http://www.mocky.io/v2/5d949e5c2f00002d008ff84c");
 let questionObject = new Question();
@@ -47,21 +51,33 @@ let quiz = new Quiz();
 questionObject.addQuests(json); 
 
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("next").addEventListener("click", function(e){
+ 
+        quiz.nextQuestion();
+    });
+
     let start = document.getElementById("start");
     
-
 start.addEventListener("click", function(e){
-    let question = document.getElementById("question").innerHTML = quiz.quests[0].question;
-    let answer1 = document.getElementById("answer1").innerHTML = quiz.quests[0].answer[0].alt;
-    let answer2 = document.getElementById("answer2").innerHTML = quiz.quests[0].answer[1].alt;
-    let answer3 = document.getElementById("answer3").innerHTML = quiz.quests[0].answer[2].alt;
-    let answer4 = document.getElementById("answer4").innerHTML = quiz.quests[0].answer[3].alt;
+    let question = document.getElementById("question").innerHTML = quiz.quests[quiz.currentQuestion].question;
+    let answer1 = document.getElementById("answer1").innerHTML = quiz.quests[quiz.currentQuestion].answer[0].alt;
+    let answer2 = document.getElementById("answer2").innerHTML = quiz.quests[quiz.currentQuestion].answer[1].alt;
+    let answer3 = document.getElementById("answer3").innerHTML = quiz.quests[quiz.currentQuestion].answer[2].alt;
+    let answer4 = document.getElementById("answer4").innerHTML = quiz.quests[quiz.currentQuestion].answer[3].alt;
     quiz.setName();
     quiz.setnoOfQuestions();
     console.log( quiz.name,'\n', quiz.noOfQuestions,'\n', question,'\n', answer1,'\n', answer2,'\n', answer3,'\n', answer4);
 });
+next.addEventListener("click", function(e){
+    let question = document.getElementById("question").innerHTML = quiz.quests[quiz.currentQuestion].question;
+    let answer1 = document.getElementById("answer1").innerHTML = quiz.quests[quiz.currentQuestion].answer[0].alt;
+    let answer2 = document.getElementById("answer2").innerHTML = quiz.quests[quiz.currentQuestion].answer[1].alt;
+    let answer3 = document.getElementById("answer3").innerHTML = quiz.quests[quiz.currentQuestion].answer[2].alt;
+    let answer4 = document.getElementById("answer4").innerHTML = quiz.quests[quiz.currentQuestion].answer[3].alt;
+    quiz.setName();
+    quiz.setnoOfQuestions();
 });
-
+});
 
 
 let choices = document.getElementById("choices");
@@ -71,8 +87,13 @@ choices.addEventListener('click', (event)=>
 
 });
 
-if(quiz.noOfQuestions >= 5){
-    
+function checkAnswer(answer){
+    if (answer == questions[currentQuestion].correct){
+        score++
+    }else{
+
+    }
+        
 }
 
 function progress(){
