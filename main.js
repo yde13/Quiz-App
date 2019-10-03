a14a7809cb968a79d7cc079a564c09f30157ed41
yde <philip.ydehed@gmail.com>
2019-10-03 12:38:41 +0200

    la till funktion som tar fram vilken checkbox som blivit kryssade och vilken som är rätt eller fel

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
 //spara värdet noofquestion och skriva ut så många frågor som spelare vill ha
 //kunna trycka på checkboxes som sparas nånstans(checked)
-//kolla ifall checkboxes är true eller false
-//spara värde så man ser score i slutet 
+//skapa en slutsida som visas när användaren svarat på det antal frågor som spelaren skrivit
 
 // skapar quiz class
 class Quiz {
@@ -57,11 +56,10 @@ questionObject.addQuests(json);
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("next").addEventListener("click", function(e){
  
-        quiz.nextQuestion();
+        
     });
 
-    let start = document.getElementById("start");
-    
+let start = document.getElementById("start");    
 start.addEventListener("click", function(e){
     document.getElementById("question").innerHTML = quiz.quests[quiz.currentQuestion].question;
     document.getElementById("answer1").innerHTML = quiz.quests[quiz.currentQuestion].answer[0].alt;
@@ -75,6 +73,11 @@ start.addEventListener("click", function(e){
 });
 
 next.addEventListener("click", function(e){
+    if(checkAnswer()) {
+        quiz.score++; 
+    };
+    console.log(checkAnswer());
+    quiz.nextQuestion();
     document.getElementById("question").innerHTML = quiz.quests[quiz.currentQuestion].question;
     document.getElementById("answer1").innerHTML = quiz.quests[quiz.currentQuestion].answer[0].alt;
     document.getElementById("answer2").innerHTML = quiz.quests[quiz.currentQuestion].answer[1].alt;
@@ -94,14 +97,15 @@ choices.addEventListener('click', (event)=>
 
 });
 
-function checkAnswer(answer){
-    if (answer == questions[currentQuestion].correct){
-        score++
-    }else{
+//skpar en function checkanswer som håller koll på vilken checkbox spelaren trycker på samt vilken checkbox som är true och false
+function checkAnswer() {
+    let checkboxes = Array.from(document.getElementsByClassName("checkboxes")).map((checkbox) => {return checkbox.checked});
 
-    }
-        
+    let correctAnswers = quiz.quests[quiz.currentQuestion].answer.map((question) => {return question.correct});
+    //gör om arrayerna checkboxes och correctanswers för att enklare kunna jämföra dem
+    return JSON.stringify(checkboxes) == JSON.stringify(correctAnswers);
 }
+
 //skriver ut vilken fråga man är på 
 function progress(){
     let progress = document.getElementById("progress");
